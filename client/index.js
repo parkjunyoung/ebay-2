e76e4247f3df10d1b8d351461b73b5b733ff6094
parkjunyoung <pjoon86@naver.com>
2017-06-21 11:59:27 +0900

    IE하위버전 Promise 적용

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Promise from 'promise-polyfill'; 
+
+// IE 하위 버전 Promise 적용
+if (!window.Promise) {
+  window.Promise = Promise;
+}
  
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
