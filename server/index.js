3c581e717e301d068c9ab4a02150903b66eebf76
parkjunyoung <pjoon86@naver.com>
2017-06-21 10:29:13 +0900

    mssql connect

--- server/index.js
+++ server/index.js
@@ -3,7 +3,32 @@ import express from 'express';
 const app = express();
  
 let port = 3000;
- 
+
+import dotenv from 'dotenv';
+dotenv.config(); // LOAD CONFIG
+
+import Sequelize from 'sequelize';
+const sequelize = new Sequelize( process.env.DATABASE , process.env.DB_USER , process.env.DB_PASSWORD , {
+  host:  process.env.DB_HOST ,
+  dialect: 'mssql',
+  dialectOptions: {
+    encrypt: true,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+    console.log('Connection has been established successfully.');
+})
+    .catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
  
 app.get('/', (req, res) => {
     res.send('Es6 export Import');
